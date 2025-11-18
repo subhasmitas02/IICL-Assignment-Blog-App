ee4abf7aa14b30a02c8742ebf63cc03537b58cd4
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2025-11-18 18:16:49 +0530

    Switch to PostgreSQL and Did minor changes

--- backend/prisma/migrations/20251117161459_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Blog" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "author" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
