5d007452a52116f4f3062d1ac3143c77ddf4f0d6
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2025-11-19 00:47:12 +0530

    Updated Drizzle config to single URL structure

--- backend/drizzle.config.js
+++ backend/drizzle.config.js
@@ -1,25 +1,11 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-import { URL } from 'url';
-
-
-const dbUrl = new URL(process.env.DATABASE_URL);
-const [user, password] = dbUrl.pathname.slice(2).split(':');
 
 export default defineConfig({
   out: './drizzle',
   schema: './db/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    host: dbUrl.hostname,
-    port: parseInt(dbUrl.port), 
-    user: user, 
-    password: password.split('@')[0], // Extract password before the host
-    database: dbUrl.pathname.slice(1).split('?')[0], // Get the database name
-    
-    // This tells the pg driver to skip certificate validation
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    url: process.env.DATABASE_URL,
   },
 });
